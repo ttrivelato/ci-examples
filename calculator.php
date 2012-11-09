9807415c54bf361802de6d1d3a16be0f5f5b401c
Thiago Trivelato <thiago.trivelato@rocket-internet.com.br>
2012-11-09 18:16:57 -0200

    Add build files and ant

--- calculator.php
+++ calculator.php
@@ -5,8 +5,7 @@ class Calculator
      * @assert (0, 0) == 0
      * @assert (0, 1) == 1
      * @assert (1, 0) == 1
-     * @assert (1, 1) == 2
-     * @assert (1, 2) == 4
+     * @assert (1, 1) == 2     
      */
     public function add($a, $b)
     {
